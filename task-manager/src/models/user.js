80e04c9cbcfa4c6c54340409dde87ba1ada32f57
Donadonzi <donadonzi@gmail.com>
2020-05-04 17:36:18 -0400

    set up automated emails using sendgrid library

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -78,7 +78,7 @@ userSchema.methods.toJSON = function () {
 /////// A method for getting a token /////////
 userSchema.methods.generateAuthToken = async function () { // Shouldn't use arrow function for 'this' bingind stuff
 	const user = this;
-	const token = jwt.sign({ _id: user._id.toString() }, 'SomeSecretForSignature');
+	const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
 	user.tokens = user.tokens.concat({ token });
 	await user.save();
 	return token;
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -3,6 +3,7 @@ const multer = require('multer');
 const sharp = require('sharp');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
+const { sendWelcomeEmail, sendUserDeleteEmail } = require('../emails/account');
 
 const router = new express.Router();
 
@@ -13,6 +14,7 @@ router.post('/users', async (req, res) => {
 
 	try {
 		await user.save();
+		sendWelcomeEmail(user.email, user.name);
 		const token = await user.generateAuthToken();
 		res.status(201).send({ user, token });
 	} catch (e) {
@@ -124,6 +126,7 @@ router.delete('/users/me', auth, async (req, res) => {
 		// 	return res.status(404).send();
 		// }
 		await req.user.remove();
+		sendUserDeleteEmail(req.user.email, req.user.name);
 		res.send(req.user);
 	}
 	catch (e) {
